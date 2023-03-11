aa35333d3dcfc0af73e0a296d068e806c8aa3a7a
GroshanCS10 <91214062+GroshanCS10@users.noreply.github.com>
2023-03-11 13:11:51 +0530

    Add files via upload

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my website</h1>
+      </header>
+      <main>
+        <p>This is a basic website built with React</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;

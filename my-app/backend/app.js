1450cd69c8235136275c39426b511e9d0f0a181f
Vinay4007 <cs20b049@iittp.ac.in>
2023-03-12 15:17:05 +0530

    Fourth commit

--- my-app/backend/app.js
+++ my-app/backend/app.js
@@ -1,57 +1,3 @@
-// const express = require('express');
-// const multer = require('multer');
-// const path = require('path');
-// const AdmZip = require('adm-zip');
-// const fs = require('fs');
-
-// const app = express();
-
-// // Create a storage object with diskStorage
-// const storage = multer.diskStorage({
-//   destination: 'uploads/',
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.post('/api/upload', upload.single('file'), (req, res) => {
-//   const file = req.file;
-//   if (!file) {
-//     return res.status(400).send('No file uploaded');
-//   }
-//   const originalName = file.originalname;
-//   const fileType = file.mimetype;
-//   console.log(fileType)
-//   if (fileType === 'application/zip' || fileType === 'application/x-zip-compressed') {
-//     console.log(fileType)
-//     const zip = new AdmZip(file.path);
-//     zip.extractAllTo(path.join(__dirname, '/', 'uploads', originalName.split('.')[0]));
-//     //zip.extractAllTo(path.join(__dirname, '..', file.path,originalName.split('.')[0]));
-//     // Delete the zip file after extraction
-//     fs.unlinkSync(file.path);
-//     return res.status(200).json({
-//       message: 'Zip file extracted successfully',
-//       originalName: originalName,
-//       fileType: fileType,
-//       filename: file.filename,
-//       path: path.join(__dirname, '..', 'uploads', originalName.split('.')[0])
-//     });
-//   } else {
-//     return res.status(200).json({
-//       message: 'File uploaded successfully',
-//       originalName: originalName,
-//       fileType: fileType,
-//       filename: file.filename,
-//       path: path.join(__dirname, '..', file.path),
-//     });
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
 
 
 
@@ -60,7 +6,7 @@ const multer = require('multer');
 const path = require('path');
 const AdmZip = require('adm-zip');
 const fs = require('fs');
-
+const { spawn } = require('child_process');
 const app = express();
 
 // Create a storage object with diskStorage
@@ -80,33 +26,54 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
   }
   const originalName = file.originalname;
   const fileType = file.mimetype;
+  console.log(fileType)
   if (fileType === 'application/zip' || fileType === 'application/x-zip-compressed') {
+    console.log(fileType)
     const zip = new AdmZip(file.path);
     const zipEntries = zip.getEntries();
-    let srcFolderPath;
-    let cmakeFilePath;
-    
-    for (const zipEntry of zipEntries) {
-      const entryPath = zipEntry.entryName.split('/');
-      if (entryPath.includes('src')) {
-        srcFolderPath = zipEntry.entryName;
-      } else if (entryPath[entryPath.length - 1] === 'CMakeLists.txt') {
-        cmakeFilePath = zipEntry.entryName;
-      }
-      if (srcFolderPath && cmakeFilePath) {
+    //let srcFolderPath;
+    //let cmakeFilePath;
+    //cc
+    let containsCppFiles = false;
+    for (let i = 0; i < zipEntries.length; i++) {
+      const zipEntry = zipEntries[i];
+      if (zipEntry.entryName.endsWith('.cpp')) {
+        containsCppFiles = true;
         break;
+      } else if (zipEntry.entryName.endsWith('.java')) {
+        // Reject zip folder if it contains Java files
+        //fs.unlinkSync(file.path);
+        //return res.status(400).send('Zip folder contains Java files');
+        containsCppFiles = false;
+      }
+      else if (zipEntry.entryName.endsWith('.py')){
+        //fs.unlinkSync(file.path);
+        //return res.status(400).send('Zip folder contains Python files');
+        containsCppFiles = false;
       }
     }
+      // cc
+    // for (const zipEntry of zipEntries) {
+    //   const entryPath = zipEntry.entryName.split('/');
+    //   if (entryPath.includes('src')) {
+    //     srcFolderPath = zipEntry.entryName;
+    //   } else if (entryPath[entryPath.length - 1] === 'CMakeLists.txt') {
+    //     cmakeFilePath = zipEntry.entryName;
+    //   }
+    //   if (srcFolderPath && cmakeFilePath) {
+    //     break;
+    //   }
+    // }
     
-    if (srcFolderPath && cmakeFilePath) {
+    if (containsCppFiles) {
       zip.extractAllTo(path.join(__dirname, '/', 'uploads', originalName.split('.')[0]));
       // Delete the zip file after extraction
       fs.unlinkSync(file.path);
-      const srcPath = path.join(__dirname, 'uploads', originalName.split('.')[0], srcFolderPath);
-      const cmakePath = path.join(__dirname, 'uploads', originalName.split('.')[0], cmakeFilePath);
-
+      //const srcPath = path.join(__dirname, 'uploads', originalName.split('.')[0], srcFolderPath);
+      //const cmakePath = path.join(__dirname, 'uploads', originalName.split('.')[0], cmakeFilePath);
+      const projectpath = path.join(__dirname, '/', 'uploads', originalName.split('.')[0])
       // Execute the Python code as a child process
-      const pythonProcess = spawn('python', ['SE_Parser.py', srcPath, cmakePath]);
+      const pythonProcess = spawn('python', ['temp.py', projectpath]);
       let output = '';
 
       pythonProcess.stdout.on('data', (data) => {
@@ -124,12 +91,13 @@ app.post('/api/upload', upload.single('file'), (req, res) => {
           originalName: originalName,
           fileType: fileType,
           filename: file.filename,
-          srcPath: srcPath,
-          cmakePath: cmakePath,
+          //srcPath: srcPath,
+          //cmakePath: cmakePath,
           output: output
         });
       });
     } else {
+      fs.unlinkSync(file.path);
       return res.status(400).json({
         message: 'Zip file does not contain the required files',
       });
@@ -150,82 +118,3 @@ app.listen(3000, () => {
 });
 
 
-
-
-// const express = require('express');
-// const multer = require('multer');
-// const path = require('path');
-// const AdmZip = require('adm-zip');
-// const fs = require('fs');
-// const antlr4 = require('antlr4');
-// const CPP14Lexer = require('./CPP14Lexer');
-// const CPP14Parser = require('./CPP14Parser');
-// const CPP14Listener = require('./CPP14Listener').CPP14Listener;
-
-// const app = express();
-
-// // Create a storage object with diskStorage
-// const storage = multer.diskStorage({
-//   destination: 'uploads/',
-//   filename: (req, file, cb) => {
-//     cb(null, file.originalname);
-//   }
-// });
-
-// const upload = multer({ storage: storage });
-
-// app.post('/api/upload', upload.single('file'), (req, res) => {
-//   const file = req.file;
-//   if (!file) {
-//     return res.status(400).send('No file uploaded');
-//   }
-//   const originalName = file.originalname;
-//   const fileType = file.mimetype;
-//   console.log(fileType)
-//   if (fileType === 'application/zip' || fileType === 'application/x-zip-compressed') {
-//     console.log(fileType)
-//     const zip = new AdmZip(file.path);
-//     zip.extractAllTo(path.join(__dirname, 'uploads', originalName.split('.')[0]));
-//     // Delete the zip file after extraction
-//     fs.unlinkSync(file.path);
-//     const filesPath = path.join(__dirname, 'uploads', originalName.split('.')[0]);
-//     const files = fs.readdirSync(filesPath);
-//     const cppFiles = files.filter((filename) => path.extname(filename) === '.cpp');
-//     const dependencies = {};
-
-//     cppFiles.forEach((filename) => {
-//       const input = fs.readFileSync(path.join(filesPath, filename)).toString();
-//       const chars = new antlr4.InputStream(input);
-//       const lexer = new CPP14Lexer.CPP14Lexer(chars);
-//       const tokens = new antlr4.CommonTokenStream(lexer);
-//       const parser = new CPP14Parser.CPP14Parser(tokens);
-//       parser.buildParseTrees = true;
-//       const tree = parser.translationunit();
-//       const listener = new CPP14Listener();
-//       antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, tree);
-//       const includes = listener.includes;
-//       dependencies[filename] = includes;
-//     });
-
-//     return res.status(200).json({
-//       message: 'Zip file extracted successfully',
-//       originalName: originalName,
-//       fileType: fileType,
-//       filename: file.filename,
-//       path: filesPath,
-//       dependencies: dependencies,
-//     });
-//   } else {
-//     return res.status(200).json({
-//       message: 'File uploaded successfully',
-//       originalName: originalName,
-//       fileType: fileType,
-//       filename: file.filename,
-//       path: path.join(__dirname, 'uploads', file.filename),
-//     });
-//   }
-// });
-
-// app.listen(3000, () => {
-//   console.log('Server started on port 3000');
-// });
---
+++ my-app/backend/temp.py
@@ -0,0 +1,60 @@
+import os
+import re
+import sys
+import json
+
+library_regex = r'#include\s*(?:<(.+?)>|\"(.+?)\")'
+find_package_regex = r'find_package\s*\(\s*([^\s\)]+)'
+link_regex = r'target_link_libraries\((.*)\)'
+
+# Get the path to the project directory
+if len(sys.argv) < 2:
+    print("Usage: python analyze_code.py <project_directory>")
+    sys.exit(1)
+project_directory = sys.argv[1]
+
+# Define sets to store the names of the identified libraries and packages
+libraries = set()
+package_names = set()
+link_lib=set()
+
+# Loop through each file in the project directory and its subdirectories
+for root, dirs, files in os.walk(project_directory):
+    for file in files:
+        # Ignore files that are not C++ source code files or CMakeLists.txt files
+        if not file.endswith(".cpp") and not file.endswith(".h") and file != "CMakeLists.txt":
+            continue
+
+        # Process C++ source code files
+        if file.endswith(".cpp") or file.endswith(".h"):
+            with open(os.path.join(root, file), "r") as f:
+                content = f.read()
+                library_names = re.findall(library_regex, content)
+                for match in library_names:
+                    for lib_name in match:
+                        if lib_name:
+                            libraries.add(lib_name)
+        # Process CMakeLists.txt files
+        else:
+            cmake_file_path = os.path.join(root, file)
+            with open(cmake_file_path, "r") as f:
+                content = f.read()
+                find_package_statements = re.findall(find_package_regex, content)
+                link_names = re.findall(link_regex, content)
+                package_names.update(find_package_statements)
+                for match in link_names:
+                    for lib_name in match.split():
+                        if lib_name:
+                            link_lib.add(lib_name)
+
+# Write the identified libraries, required packages, and target-link-libraries to a JSON file
+output_file = "observations.json"
+data = {"identified_header_libraries": list(libraries),
+        "required_packages": list(package_names),
+        "target_link_libraries": list(filter(lambda lib: lib not in package_names, link_lib))}
+
+with open(output_file, "w") as f:
+    json.dump(data, f, indent=4)
+
+# Print the path to the output file
+print(f"Results written to {output_file}")

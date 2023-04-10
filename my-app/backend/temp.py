35dd0c788ceee58ff747dbe7d78fad769a593edf
hiranmai19 <CS20B014@iittp.ac.in>
2023-04-10 10:33:44 +0530

    versions added

--- my-app/backend/temp.py
+++ my-app/backend/temp.py
@@ -1,4 +1,3 @@
-#importing necessary libraries
 import os
 import re
 import sys
@@ -6,8 +5,10 @@ import json
 
 # Regular expressions to identify #include statements, find_package statements, and target_link_libraries statements
 library_regex = r'#include\s*(?:<(.+?)>|\"(.+?)\")'
-find_package_regex = r'find_package\s*\(\s*([^\s\)]+)'
+find_package_regex = r'find_package\s*\(\s*([^\s\)]+)\)'
 link_regex = r'target_link_libraries\((.*)\)'
+cmake_minimum_regex = r'cmake_minimum_required\(VERSION\s*(\d+\.\d+(\.\d+)?)\)'
+set_cpp_standard_regex = r'set\(CMAKE_CXX_STANDARD\s+(\d+)\)'
 
 # Checking if a project directory is specified as a command-line argument
 if len(sys.argv) < 2:
@@ -18,17 +19,19 @@ project_directory = sys.argv[1]   # Get the path to the project directory
 # Define sets to store the names of the identified libraries and packages
 libraries = set()
 package_names = set()
-link_lib=set()
+link_lib = set()
+cmake_version_matches = []
+cpp_standard_matches = []
 
 # Loop through each file in the project directory and its subdirectories
 for root, dirs, files in os.walk(project_directory):
     for file in files:
         # Ignore files that are not C++ source code files or CMakeLists.txt files
-        if not file.endswith(".cpp") and not file.endswith(".cc") and not file.endswith(".cxx") and not file.endswith(".h") and not file.endswith(".hpp") and not file.endswith(".hh") and not file.endswith(".hxx") and not file.endswith(".cu") and not file.endswith(".cuh") and file != "CMakeLists.txt":
+        if not any(file.endswith(extension) for extension in ['.cpp', '.cc', '.cxx', '.h', '.hpp', '.hh', '.hxx', '.cu', '.cuh', 'CMakeLists.txt']):
             continue
 
         # Process C++ source code files
-        if file.endswith(".cpp") or file.endswith(".cc") or file.endswith(".cxx") or file.endswith(".cu") or file.endswith(".h") or file.endswith(".hpp") or file.endswith(".hh") or file.endswith(".hxx") or file.endswith(".cuh"):
+        if any(file.endswith(extension) for extension in ['.cpp', '.cc', '.cxx', '.cu', '.h', '.hpp', '.hh', '.hxx', '.cuh']):
             with open(os.path.join(root, file), "r") as f:
                 content = f.read()
                 library_names = re.findall(library_regex, content)
@@ -43,6 +46,8 @@ for root, dirs, files in os.walk(project_directory):
                 content = f.read() # Reads the contents of the file
                 find_package_statements = re.findall(find_package_regex, content) # Finds all find_package statements in the file
                 link_names = re.findall(link_regex, content)
+                cmake_version_matches += re.findall(cmake_minimum_regex, content)
+                cpp_standard_matches += re.findall(set_cpp_standard_regex, content)
                 package_names.update(find_package_statements)  # Add any package names to the set of required packages
                 # Loops through the matches and add any library names to the set of target-link-libraries
                 for match in link_names:
@@ -50,11 +55,14 @@ for root, dirs, files in os.walk(project_directory):
                         if lib_name:
                             link_lib.add(lib_name)
 
-# Write the identified libraries, required packages, and target-link-libraries to a JSON file
+# Write the identified libraries, required packages, target-link-libraries, cmake minimum required version, and C++ standard version to
+
 output_file = "observations.json"
 data = {"identified_header_libraries": list(libraries),
         "required_packages": list(package_names),
-        "target_link_libraries": list(filter(lambda lib: lib not in package_names, link_lib))}
+        "target_link_libraries": list(filter(lambda lib: lib not in package_names, link_lib)),
+        "cmake_minimum_required_version": cmake_version_matches[0] if len(cmake_version_matches) > 0 else "",
+        "cpp_standard": cpp_standard_matches[0] if len(cpp_standard_matches) > 0 else ""}
 
 with open(output_file, "w") as f:
     json.dump(data, f, indent=4)
--- my-app/src/display.js
+++ my-app/src/display.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 export default function DataDisplay(props) {
-  const { identified_header_libraries, required_packages, target_link_libraries } = props.data;
+  const { identified_header_libraries, required_packages, target_link_libraries, cmake_minimum_required_version, cpp_standard } = props.data;
+
 
   return (
     <div className="div-container">
       <div className="box">
         <div className='subbox'>
-        <h2>Identified Header Libraries</h2>
+          <h2>Identified Header Libraries</h2>
         </div>
         <ul>
           {identified_header_libraries.map((library, index) => (
@@ -17,8 +18,8 @@ export default function DataDisplay(props) {
       </div>
 
       <div className="box">
-      <div className='subbox'>
-        <h2>Required Packages</h2>
+        <div className='subbox'>
+          <h2>Required Packages</h2>
         </div>
         <ul>
           {required_packages.map((pkg, index) => (
@@ -28,13 +29,23 @@ export default function DataDisplay(props) {
       </div>
 
       <div className="box">
-      <div className='subbox'>
-        <h2>Target Link Libraries</h2>
+        <div className='subbox'>
+          <h2>Target Link Libraries</h2>
         </div>
         <ul>
           {target_link_libraries.map((library, index) => (
             <li key={index} className='list'>{library}</li>
           ))}
+        </ul>
+      </div>
+
+      <div className="box">
+        <div className='subbox'>
+          <h2>Versions</h2>
+        </div>
+        <ul>
+        <li className='list'>CMake version: {cmake_minimum_required_version}</li>
+        <li className='list'>C++ standard: {cpp_standard}</li>
         </ul>
       </div>
     </div>
